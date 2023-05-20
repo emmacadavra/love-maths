5df814daa49ed73ffa4709ab1b8c573708568103
emmacadavra <>
2023-05-20 17:13:46 +0000

    Add js code to check answer and respond to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,8 +34,22 @@ function runGame(gameType) {
     }
 }
 
+/**
+ * Checks the answer agaist the first element in
+ * the returned calculateCorrectAnswer array
+ */
 function checkAnswer() {
-    
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+
+    if (isCorrect) {
+        alert("Yay! You got it right! :D");
+    } else {
+        alert(`Boo! You answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+    }
+
+    runGame(calculatedAnswer[1]);
 }
 
 /**
@@ -43,8 +57,8 @@ function checkAnswer() {
  * directly from the dom, and returns the correct answer.
  */
 function calculateCorrectAnswer() {
-    let operand1 = parseInt(document.getElementById("operand1").innerText);
-    let operand2 = parseInt(document.getElementById("operand2").innerText);
+    let operand1 = parseInt(document.getElementById("operand-one").innerText);
+    let operand2 = parseInt(document.getElementById("operand-two").innerText);
     let operator = document.getElementById("operator").innerText;
 
     if (operator === "+") {
